d0d06002486eb5cf3585e14d29ea0f1efbd10693
kellywsq03 <kellywsq03@gmail.com>
2026-08-07 12:53:55 +0800

    Initial Commit

---
+++ respondR/respondRApp.swift
@@ -0,0 +1,18 @@
+//
+//  respondRApp.swift
+//  respondR
+//
+//  Created by Kelly Wang on 7/8/26.
+//
+
+import SwiftUI
+
+@main
+struct respondRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
