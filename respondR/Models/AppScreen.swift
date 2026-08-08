1b6595c15f823fe25a5c65ec1f9f684c44d06910
kellywsq03 <kellywsq03@gmail.com>
2026-08-08 17:01:32 +0800

    add new live scene type

--- respondR/Models/AppScreen.swift
+++ respondR/Models/AppScreen.swift
@@ -4,5 +4,9 @@ enum AppScreen: Equatable {
     case phaseSelection
     case layoutSelection
     case liveScene(layout: Int)
+<<<<<<< HEAD
     case phaseTwo
+=======
+    case liveSceneAsset(assetName: String)
+>>>>>>> 2838685 (add new live scene type)
 }
