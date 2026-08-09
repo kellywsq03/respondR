0f26d664d0eabc60f8c517b6956efd6b4a2dc927
kellywsq03 <kellywsq03@gmail.com>
2026-08-09 17:54:32 +0800

    remove merge conflicts

--- respondR/Models/AppScreen.swift
+++ respondR/Models/AppScreen.swift
@@ -4,9 +4,6 @@ enum AppScreen: Equatable {
     case phaseSelection
     case layoutSelection
     case liveScene(layout: Int)
-<<<<<<< HEAD
     case phaseTwo
-=======
     case liveSceneAsset(assetName: String)
->>>>>>> 2838685 (add new live scene type)
 }
